e94aa657b635805101be752110d5418959e666f8
kawnur <kawnur@yandex.ru>
2025-07-06 15:52:01 +0300

    Functions stretched along horizontal

--- complementary_polynom.py
+++ complementary_polynom.py
@@ -1,11 +1,13 @@
+import math
+import operator
 import time
 from copy import deepcopy
 from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 DELIMITER = '#' * 180
 
@@ -275,39 +277,41 @@ def fix_and_search_coeffs(coeffs):
     build_graph_group(coeffs_full, 0.1, 0.001)
 
 
-def main():
-    coeffs_1 = [-1, -3, 1, 6, 0]
-    first_derivative_coeffs = get_first_derivative_coeffs(coeffs_1)
-    second_derivative_coeffs = get_second_derivative_coeffs(coeffs_1)
+def get_d_interval(a, b, c):
+    a1 = (27 * 27 * pow(a, 4)) / 4
+    a2 = 27 * pow(a, 2) * pow(b, 3)
+    a3 = 27 * pow(a, 6) * pow(c, 3) - 27 * pow(a, 2) * pow(b, 2) * pow(c, 2) - pow(b, 6) + pow(b, 2)
 
-    # play_with_coeffs(coeffs_1)
-    # fix_and_search_coeffs(coeffs_1)
+    discriminant = a2 * a2 - 4 * a1 * a3
+
+    if discriminant < 0:
+        return None
+
+    elif discriminant == 0:
+        d = -a2 / (2 * a1)
+        return [d, d]
+
+    else:
+        d1 = -a2 - sqrt(discriminant) / (2 * a1)
+        d2 = -a2 + sqrt(discriminant) / (2 * a1)
+        return [d1, d2]
+
+
+def get_complementary_polynom_coeffs(coeffs):
+    # for i in range(-10, 10, 1):
+    #     coeffs[0] = i
+    #
+    #     if coeffs[0] == 0:
+    #         continue
 
     # one extremum condition
     # Q = 0 (p = q = 0) in Cardano's formula
-    # fix a, from one extremum condition get expressions for b and c
-    # from 'same extremum' condition get qubic equation and find d
-    # coeffs_full = [deepcopy(coeffs_1)]
 
-    extremums = sorted(get_extremums(coeffs_1))
-    x = max(extremums)
+    extremums = sorted(get_extremums(coeffs))
+    x = max([[i, polynome(coeffs, i)] for i in extremums], key=operator.itemgetter(1))[0]
     print("x:", x)
 
-    coeffs_full = [deepcopy(coeffs_1)]
-
-    a1 = coeffs_1[0]
-    b1 = coeffs_1[1]
-    c1 = coeffs_1[2]
-    d1 = coeffs_1[3]
-
-    # a3 = a1 - 1
-    # distance = 2
-    #
-    # for b2 in range(-distance, distance, 1):
-    #     b3 = b1 + b2
-    #     c3 = (3 * pow(b3, 2)) / (8 * a3)
-    #     d3 = pow(b3, 3) / (16 * pow(a3, 2))
-    #     coeffs_full.append(deepcopy([a3, b3, c3, d3]))
+    coeffs_full = [deepcopy(coeffs)]
 
     a3 = (-3 / 20) * x
     b3 = (3/5) * pow(x, 2)
@@ -317,48 +321,64 @@ def main():
     print_custom('a3:', a3, 'b3:', b3, 'c3:', c3, 'd3:', d3)
 
     coeffs_3 = deepcopy([a3, b3, c3, d3])
-    coeffs_full.append(coeffs_3)
 
-    print_custom("coeffs_full:", coeffs_full)
+    # move graph down to zero
+    e3 = polynome(coeffs_3, x)
+    coeffs_3.append((-1) * e3)
+    # coeffs_full.append(coeffs_3)
 
-    print("#" * 50)
-    dc = get_first_derivative_coeffs(coeffs_3)
-    print(qubic_equation_roots(dc))
-    print("#" * 50)
+    # print_custom("coeffs_full:", coeffs_full)
+
+    # build_graph_group(coeffs_full, 0.1, 0.001)
+
+    return coeffs_3
+
+
+def main():
+    coeffs_1 = [-1, -3, 1, 6, 0]
+
+    # play_with_coeffs(coeffs_1)
+    # fix_and_search_coeffs(coeffs_1)
+    # get_complementary_polynom(coeffs_1)
+
+    # search for polynoms with 3 extremums
+    # for a in range(-2, -1, 1):
+    #     b = 0
+    #     # for b in range(-5, -1, 1):
+    #     aa = -27 * pow(a, 6)
+    #     bb = 27 * pow(a, 2) * pow(b, 2)
+    #     cc = 0
+    #     dd = pow(b, 6) - pow(b, 2)
+    #
+    #     c_zeros = qubic_equation_roots(deepcopy([aa, bb, cc, dd]))
+    #     print(c_zeros)
+    #
+    #     # for c in range(1, 5, 1):
+    #     #     if get_d_interval(a, b, c):
+    #     #         [d1, d2] = get_d_interval(a, b, c)
+    #     #         print(a, b, c, d1, d2)
+    #
+    #         # for d in range(math.ceil(d1), math.floor(d2), 1):
+    #         #     get_complementary_polynom(deepcopy([a, b, c, d]))
+
+    coeffs_full = [deepcopy(coeffs_1)]
+    coeffs_full.append(get_complementary_polynom_coeffs(coeffs_1))
+
+    rate = 0.5
+    step = 0.1
+
+    for k in range(0, 5, 1):
+        base = rate - k * step
+        print("base:", base)
+        coeffs = [coeffs_1[i] * pow(base, 4 - i) for i in range(len(coeffs_1))]
+
+        coeffs_full.append(coeffs)
+        coeffs_full.append(get_complementary_polynom_coeffs(coeffs))
 
     build_graph_group(coeffs_full, 0.1, 0.001)
 
-
-
-
-
-    # extremums = sorted(get_extremums(coeffs_1))
-    # x = max(extremums)
-    # print("x:", x)
-    #
-    # a = coeffs_1[0]
-    #
-    # c3 = 1
-    # c2 = 2 * x * pow(27 * a, 1/3)
-    # c1 = 3 * pow(x, 2) * np.emath.sqrt(3 * a) * pow(27 * a, 1/6)
-    # c0 = 4 * a * pow(x, 3)
-    # t = qubic_equation_roots([c3, c2, c1, c0])
-    # print("t:", t)
-    #
-    # ds = [pow(i, 1/3) for i in t]
-    # print("ds:", ds)
-    #
-    # for d in ds:
-    #     c = pow(27 * a * d * d, 1/3)
-    #     b = np.emath.sqrt(3 * a * c)
-    #     coeffs_full.append(deepcopy([a, b, c, d]))
-    #
-    # print_custom("coeffs_full:", coeffs_full)
-    #
-    # build_graph_group(coeffs_full, 0.1, 5)
-
-    # convex condition
-    # zeros = quadratic_equation_roots()
+    # for coeffs in coeffs_full:
+    #     get_complementary_polynom(coeffs)
 
 
 if __name__ == '__main__':
